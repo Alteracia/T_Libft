553b570b76d1440d70b79e91de068cba98f1c97a
Alteracia <48139187+Alteracia@users.noreply.github.com>
2019-05-13 15:56:29 +0300

    usage add

--- gcc.sh
+++ gcc.sh
@@ -1,5 +1,9 @@
+if [ -z "$1" ]; then
+	echo "Usage: bash gcc.sh [your_path_without_~/]"
+	exit 0;
+fi
 make re --directory ~/$1
 gcc -Wall -Wextra -Werror -O0 -g -Werror -I ~/$1 main.c -L ~/$1 -lft
-~/Applications/valgrind/bin/valgrind --leak-check=yes ./a.out
+./a.out
 rm a.out
 make --directory ~/$1 fclean
